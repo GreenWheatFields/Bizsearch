c6fe5a7d695efd6f5c4c8a13fba517db0cf96ddc
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-04-26 18:21:37 -0400

    promising progress on alabama brainstorming

--- src/Alabama.java
+++ src/Alabama.java
@@ -10,11 +10,26 @@ import java.io.IOException;
 public class Alabama {
 
     public static void main(String[] args) throws IOException {
-
+        int count = 0;
         Document doc = Jsoup.connect(urlsAndKeys.AlabamaUrl()).userAgent("Mozilla/5.0").get();
+        //Document doc1 = doc;
         Elements natureOfBusiness = doc.select("#block-sos-content > div > div > div > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(11) > td.aiSosDetailValue");
-        String test = natureOfBusiness.outerHtml();
-        System.out.println(test);
+        String test = natureOfBusiness.text();
+        //System.out.println(test);
+        String bizInfo = doc.select("#block-sos-content > div > div > div > table:nth-child(2) > tbody:nth-child(2)").text();
+        //System.out.println(bizInfo);
+        Elements inspectors = doc.getElementsByClass("aiSosDetailDesc");
+
+        for (Element e : inspectors){
+            System.out.println(e.text() + "\n");
+        }
+
+
+
+
+        //System.out.println(inspectors);
+        //System.out.println(inspectors);
+
 
     }
 }
