885b2139964eb756a35abf87c7957a4553cbc38b
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-04-26 20:55:11 -0400

    2d array of all relevant data.

--- src/Alabama.java
+++ src/Alabama.java
@@ -2,6 +2,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 
@@ -11,7 +13,9 @@ public class Alabama {
 
     public static void main(String[] args) throws IOException {
         int count = 0;
-        Document doc = Jsoup.connect(urlsAndKeys.AlabamaUrl()).userAgent(RandomUserAgent.getRandomUserAgent()).get();
+        String URL = urlsAndKeys.AlabamaUrl();
+        System.out.println(URL);
+        Document doc = Jsoup.connect(URL).userAgent(RandomUserAgent.getRandomUserAgent()).get();
         //Document doc1 = doc;
         Elements natureOfBusiness = doc.select("#block-sos-content > div > div > div > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(11) > td.aiSosDetailValue");
         String test = natureOfBusiness.text();
@@ -20,15 +24,26 @@ public class Alabama {
         //System.out.println(bizInfo);
         Elements inspectors = doc.getElementsByClass("aiSosDetailDesc");
         Elements inspectors2 = doc.getElementsByClass("aiSosDetailValue");
-        //for (Element e : inspectors Element a: inspectors2){
-        //}}
-        System.out.println(inspectors.size());
+        String[][] table = new String[inspectors.size()][inspectors2.size()];
+        for (Element e : inspectors) {
+            //System.out.println(e.text());
+            table[0][count] = e.text();
+            count++;
+        }
+        count = 0;
+        for (Element p : inspectors2){
+            table[1][count] = p.text();
+            count++;
+        }
+
+        for (int i = 0; i < inspectors.size()-1; i++){
+                System.out.println(table[0][i] + " : " + table[1][i]);
+
+        }
 
 
-
-        //System.out.println(inspectors);
-        //System.out.println(inspectors);
-
-
-    }
+    //System.out.println(inspectors);
+    //System.out.println(inspectors);
 }
+}
+
