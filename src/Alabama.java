e779dc9ad39f6bdbc46bf1234ff693e4c6508fe8
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-04-28 14:55:05 -0400

    can add to a csv, need to configure proxy and better parse data.

--- src/Alabama.java
+++ src/Alabama.java
@@ -4,6 +4,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
+import javax.print.Doc;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -14,50 +16,42 @@ public class Alabama {
         String URL = urlsAndKeys.AlabamaUrl();
         System.out.println(URL);
         Document doc = Jsoup.connect(URL).userAgent(RandomUserAgent.getRandomUserAgent()).get();
-        //Document doc1 = doc;
-        Elements natureOfBusiness = doc.select("#block-sos-content > div > div > div > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(11) > td.aiSosDetailValue");
-        String test = natureOfBusiness.text();
-        //System.out.println(test);
-        String bizInfo = doc.select("#block-sos-content > div > div > div > table:nth-child(2) > tbody:nth-child(2)").text();
-        //System.out.println(bizInfo);
-        Elements inspectors = doc.getElementsByClass("aiSosDetailDesc");
-        Elements inspectors2 = doc.getElementsByClass("aiSosDetailValue");
-        String[][] table = new String[inspectors.size()][inspectors2.size()];
-        for (Element e : inspectors) {
+        Elements description = doc.getElementsByClass("aiSosDetailDesc");
+        Elements value = doc.getElementsByClass("aiSosDetailValue");
+        String name = doc.select("#block-sos-content > div > div > div > table:nth-child(2) > thead:nth-child(1) > tr > td").text();
+        String[][] table = new String[description.size() +1][value.size() + 1];
+        for (Element e : description) {
             //System.out.println(e.text());
             table[0][count] = e.text();
             count++;
         }
         count = 0;
-        for (Element p : inspectors2){
+        for (Element p : value){
             table[1][count] = p.text();
             count++;
         }
+        //BufferedWriter csvWriter = new BufferedWriter(
+        //        new FileWriter("Alabama.csv", true));
 
-        for (int i = 0; i < inspectors.size()-1; i++){
-            System.out.println(table[0][i] + " : " + table[1][i]);
 
+        FileWriter csvWriter = new FileWriter("Alabama.csv",true);
+            //csvWriter.append("URL,Country,State,Name,ID#,Type,Status,Disolvation Date,Formation Date,Place of Formation,Principal Adderess, Prinicipal Mailing Address, Registered Agent Name, Registered Office Street Address, Registered Office Mailing Adderess, Nature of Buisness, Capital Authorized, Capital Paid In,Incorporator Name, Incorporator Street Adderess, Incorporator Mailing Adderess,\n");
+        for (int i = 0; i < description.size(); i++){
+           System.out.println(table[0][i] + " : " + table[1][i]);
         }
-        FileWriter csvWriter = new FileWriter("Alabama.csv");
-        csvWriter.append("State,");
-        csvWriter.append("Country,");
-        csvWriter.append("ID Number");
-        csvWriter.append(",");
-        csvWriter.append("Entity Type");
-        csvWriter.append(",");
-        csvWriter.append("Principal Address,");
-        csvWriter.append("\n");
-        csvWriter.append("AL,");
-        csvWriter.append("USA,");
-        csvWriter.append(table[1][0]).append(",");
-        csvWriter.append(table[1][1]).append(",");
-        csvWriter.append(table[1][2]).append("\n");
-        csvWriter.flush();
-        csvWriter.close();
+            csvWriter.append(URL).append(",");
+            csvWriter.append("USA").append(",");
+            csvWriter.append("Al").append(",");
+            csvWriter.append(name + ",");
+            csvWriter.append(table[1][0]).append(",\n");
+            csvWriter.flush();
+            csvWriter.close();
 
-     //   System.out.println(table[0][5] + table[1][5]);
-    //System.out.println(inspectors);
-    //System.out.println(inspectors);
+
+
+     //System.out.println(table[0][5] + table[1][5]);
+    //System.out.println(description);
+    //System.out.println(description);
 }
 
 }
