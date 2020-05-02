87af321e5f0032b91aa5e929424a9e69360e2cce
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-05-02 13:41:47 -0400

    added 2d array for ip/port

--- src/FreeProxyScraper.java
+++ src/FreeProxyScraper.java
@@ -6,26 +6,24 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 
 public class FreeProxyScraper {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://free-proxy-list.net/").userAgent(RandomUserAgent.getRandomUserAgent()).get();
-        Elements test = doc.select("#proxylisttable > tbody");
-        int count =0;
-        String[] motherList = new String[20];
-        for (int i = 0; i < motherList.length; i++){
+        String[][] proxyList = new String[20][20];
+        for (int i = 0; i < proxyList.length; i++){
             String test2 = doc.select("#proxylisttable > tbody > tr:nth-child(" + i +1 +")").text();
             String[]p = test2.split(" ");
-            motherList[i] = p[0] + ":" + p[3];
-            System.out.println(motherList[i]);
-
+            proxyList[0][i] = p[0];
+            proxyList[1][i] = p[1];
         }
-
-        /*System.setProperty(p[1],p[3]);
+        System.out.println(proxyList[0][0] + " " + proxyList[1][0]);
+        System.setProperty(proxyList[0][0],proxyList[0][1]);
         URL url = new URL("http://arc-sos.state.al.us/cgi/corpdetail.mbr/detail?page=number&num1=");
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
         con.setConnectTimeout(1000);
-        System.out.println(con.getResponseCode()); */
+        System.out.println(con.getResponseCode());
     }
 
 
